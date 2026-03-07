7c28e46ee3b338e557bc23c22b0f645dd95c578c
Fixit <fixit.zarzis@gmail.com>
2026-03-07 09:24:49 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,7 @@ import {
   LayoutDashboard, Users, Package, Wrench, ShoppingCart,
   Truck, Shield, DollarSign, Receipt, Tag, Bell,
   ClipboardList, Settings, Menu, X, ChevronDown,
-  LogOut, Warehouse, ScrollText, ShoppingBag
+  LogOut, Warehouse, ScrollText, ShoppingBag, Wifi
 } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
