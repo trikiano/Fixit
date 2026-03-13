5e20915dc8349eafb2ee39e0f3d94995a5ed54cb
Fixit <fixit.zarzis@gmail.com>
2026-03-13 16:40:15 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import SupplierInvoices from './pages/SupplierInvoices';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
