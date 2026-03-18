a39b98eda0e20d1faafc194ac81eabaa6cfcb945
Fixit <fixit.zarzis@gmail.com>
2026-03-18 17:19:29 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import SupplierInvoices from './pages/SupplierInvoices';
+import Services from './pages/Services';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
