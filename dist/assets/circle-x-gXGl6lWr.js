1e7132c8d8257172c24a7d08e514501861167d58
Fixit <fixit@fixithub.net>
2026-04-18 12:29:53 +0100

    feat: include dist/ for Hostinger deployment + remove build step

---
+++ dist/assets/circle-x-gXGl6lWr.js
@@ -0,0 +1,11 @@
+import{b as c}from"./index-CsFFp-Dx.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]],i=c("CircleCheck",e);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],a=c("CircleX",r);export{i as C,a};
---
+++ dist/assets/index-CtlYlah8.js
@@ -0,0 +1,1 @@
+function r(e){return"/"+e.replace(/ /g,"-")}export{r as c};
